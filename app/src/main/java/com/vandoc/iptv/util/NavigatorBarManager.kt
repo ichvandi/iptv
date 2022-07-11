82be711293486b7461bdd305608e26dba19be0da
Achmad Ichsan <achmad.ichsan@dana.id>
2022-07-12 00:07:36 +0700

    refactor to composable

--- app/src/main/java/com/vandoc/iptv/util/NavigatorBarManager.kt
+++ app/src/main/java/com/vandoc/iptv/util/NavigatorBarManager.kt
@@ -1,23 +1,22 @@
 package com.vandoc.iptv.util
 
-import android.animation.ObjectAnimator
-import android.app.Activity
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.platform.LocalContext
-import com.vandoc.iptv.R
 
 /**
  * @author Achmad Ichsan
  * @version NavigatorBarManager, v 0.1 11/07/22 22.53 by Achmad Ichsan
  */
+@Composable
+fun NavigatorBarChangeColorManager(id: Int?) {
+    val context = LocalContext.current
+    val activity = remember { context.findActivity() }
+    val formerColor = rememberSaveable { activity?.window?.navigationBarColor ?: 0 }
 
-class NavigatorBarManager(private val activity: Activity) {
-    private val formerColor = activity.window.navigationBarColor
-
-    fun changeColor(id: Int?) {
-        activity.run {
-            window.navigationBarColor =
-                id?.let { getColor(it) } ?: run { formerColor }
-        }
+    activity?.run {
+        window.navigationBarColor =
+            id?.let { getColor(it) } ?: run { formerColor }
     }
 }
