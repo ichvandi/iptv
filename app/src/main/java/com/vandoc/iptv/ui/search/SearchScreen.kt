c4423d7a2133a2758a2b47632aa0538a26a97988
Ichvandi Okta Maulana <ichvandi.maulana@stockbit.com>
2022-08-24 22:39:34 +0700

    Add fab to search screen

--- app/src/main/java/com/vandoc/iptv/ui/search/SearchScreen.kt
+++ app/src/main/java/com/vandoc/iptv/ui/search/SearchScreen.kt
@@ -1,11 +1,13 @@
 package com.vandoc.iptv.ui.search
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.FilterAlt
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -38,62 +40,82 @@ fun SearchScreen(
     viewModel: SearchViewModel = hiltViewModel()
 ) {
     var queryState by rememberSaveable { mutableStateOf(query) }
+    var shouldShowFAB by rememberSaveable { mutableStateOf(true) }
 
     LaunchedEffect(queryState) {
         viewModel.setAction(SearchAction.Search(queryState))
     }
 
-    val uiState = viewModel.uiState
-    CoordinatorLayout(
-        toolbarHeight = TOOLBAR_HEIGHT_IN_DP,
-        toolbar = { toolbarOffsetHeightPx ->
-            TopAppBar(
-                title = {
-                    SearchBar(
-                        initialValue = queryState,
-                        hint = "Search channel name",
-                        onSearch = { queryState = it },
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                },
-                navigationIcon = {
-                    IconButton(onClick = { navigator.navigateUp() }) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_baseline_chevron_left_24),
-                            contentDescription = ""
-                        )
-                    }
-                },
-                modifier = Modifier
-                    .height(TOOLBAR_HEIGHT_IN_DP)
-                    .offset {
-                        IntOffset(
-                            x = 0,
-                            y = toolbarOffsetHeightPx.roundToInt()
-                        )
-                    }
-            )
-        },
-        content = {
-            if (viewModel.uiState.isLoading) {
-                Box(modifier = Modifier.fillMaxSize()) {
-                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+    Scaffold(
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = {
+            AnimatedVisibility(
+                visible = shouldShowFAB,
+                enter = slideInVertically(initialOffsetY = { it }),
+                exit = slideOutVertically(targetOffsetY = { it + it / 2 })
+            ) {
+                FloatingActionButton(onClick = {
+
+                }) {
+                    Icon(imageVector = Icons.Outlined.FilterAlt, contentDescription = null)
                 }
             }
-
-            if (uiState.channels.isNotEmpty()) {
-                GridChannel(
-                    channels = uiState.channels,
-                    columns = GridCells.Fixed(2),
-                    onItemClicked = { navigator.navigate(DetailScreenDestination(it)) },
-                    contentPadding = PaddingValues(
-                        start = 8.dp,
-                        top = TOOLBAR_HEIGHT_IN_DP + 8.dp,
-                        end = 8.dp,
-                        bottom = 8.dp
-                    )
-                )
-            }
         }
-    )
+    ) {
+        CoordinatorLayout(
+            toolbarHeight = TOOLBAR_HEIGHT_IN_DP,
+            toolbar = { toolbarOffsetHeightPx ->
+                shouldShowFAB = toolbarOffsetHeightPx == 0f || toolbarOffsetHeightPx > -100
+
+                TopAppBar(
+                    title = {
+                        SearchBar(
+                            initialValue = queryState,
+                            hint = "Search channel name",
+                            onSearch = { queryState = it },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = { navigator.navigateUp() }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_baseline_chevron_left_24),
+                                contentDescription = ""
+                            )
+                        }
+                    },
+                    modifier = Modifier
+                        .height(TOOLBAR_HEIGHT_IN_DP)
+                        .offset {
+                            IntOffset(
+                                x = 0,
+                                y = toolbarOffsetHeightPx.roundToInt()
+                            )
+                        }
+                )
+            },
+            content = {
+                if (viewModel.uiState.isLoading) {
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                    }
+                }
+
+                if (viewModel.uiState.channels.isNotEmpty()) {
+                    GridChannel(
+                        channels = viewModel.uiState.channels,
+                        columns = GridCells.Fixed(2),
+                        onItemClicked = { navigator.navigate(DetailScreenDestination(it)) },
+                        contentPadding = PaddingValues(
+                            start = 8.dp,
+                            top = TOOLBAR_HEIGHT_IN_DP + 8.dp,
+                            end = 8.dp,
+                            bottom = 8.dp
+                        )
+                    )
+                }
+            },
+            modifier = Modifier.padding(it)
+        )
+    }
 }
