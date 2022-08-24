bfe394384c9b6ba9c09c48df5bfd90e5aa773b63
Ichvandi Okta Maulana <ichvandi.maulana@stockbit.com>
2022-08-24 21:22:12 +0700

    Add loading on home screen

--- app/src/main/java/com/vandoc/iptv/ui/home/HomeScreen.kt
+++ app/src/main/java/com/vandoc/iptv/ui/home/HomeScreen.kt
@@ -1,10 +1,14 @@
 package com.vandoc.iptv.ui.home
 
 import android.widget.Toast
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -37,6 +41,12 @@ fun MainScreen(
     viewModel.event.observeWithLifecycle {
         when (it) {
             is HomeEvent.ShowToast -> Toast.makeText(context, it.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    if (viewModel.uiState.isLoading) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
         }
     }
 
