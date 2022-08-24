2280d8a1774c90ad301559688a8c636eaad022ad
Ichvandi Okta Maulana <ichvandi.maulana@stockbit.com>
2022-08-24 15:45:46 +0700

    Add dummy channel

--- app/src/main/java/com/vandoc/iptv/util/Dummy.kt
+++ app/src/main/java/com/vandoc/iptv/util/Dummy.kt
@@ -1,5 +1,6 @@
 package com.vandoc.iptv.util
 
+import com.vandoc.iptv.data.model.local.Channel
 import com.vandoc.iptv.data.model.local.ChannelMini
 
 /**
@@ -19,3 +20,69 @@ val DUMMY_CHANNELS = List(2) {
 }
 
 const val DUMMY_SECTION = "Stream all channels"
+
+val DUMMY_CHANNELS = List(2) {
+    Channel(
+        id = "$it",
+        name = "Channel $it",
+        nativeName = "Channel Native $it",
+        network = "Network $it",
+        launched = "2022-01-24",
+        closed = null,
+        isNsfw = it == 0,
+        replacedBy = null,
+        website = "https://google.com/",
+        logo = "https://www.lyngsat.com/logo/tv/ii/insert_id.png",
+        region = "Region $it",
+        country = "Country $it",
+        flag = "https://raw.githubusercontent.com/emcrisostomo/flags/master/png/256/SG.png",
+        subdivision = "Subdivision $it",
+        broadcastAreas = Channel.BroadcastAreas(
+            region = listOf("A", "B", "C"),
+            country = listOf("A", "B", "C"),
+            subdivision = listOf("A", "B", "C"),
+        ),
+        categories = listOf("Cat 1", "Cat 2", "Cat 3"),
+        languages = listOf("Lang 1", "Lang 2", "Lang 3"),
+        streams = listOf(
+            Channel.Stream(
+                url = "",
+                status = "online",
+                width = 1920,
+                height = 1080,
+                resolution = "FHD",
+            ),
+            Channel.Stream(
+                url = "",
+                status = "timeout",
+                width = 1920,
+                height = 1080,
+                resolution = "FHD",
+            ),
+            Channel.Stream(
+                url = "",
+                status = "online",
+                width = 1280,
+                height = 720,
+                resolution = "HD",
+            )
+        ),
+        guides = listOf(
+            Channel.Guide(
+                url = "",
+                website = "",
+                language = "English"
+            ),
+            Channel.Guide(
+                url = "",
+                website = "",
+                language = "English"
+            ),
+            Channel.Guide(
+                url = "",
+                website = "",
+                language = "English"
+            )
+        )
+    )
+}
